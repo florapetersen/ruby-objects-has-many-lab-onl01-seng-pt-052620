a1ce46e26d37c4a10b628a10fe131cf94fc9549e
Flora Petersen <floramarina@comcast.net>
2020-06-24 22:54:44 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
 class Post
-  attr_accessor :author, :name
+  attr_accessor :author, :title
 
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     save
   end
 
