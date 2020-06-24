7a96dbc2fcbe0307bf91f094b0a2221d481535ca
Flora Petersen <floramarina@comcast.net>
2020-06-24 22:55:38 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,15 @@
 class Author
   attr_accessor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
   end
 
   def add_post(post)
     post.author = self
+    @@post_count += 1
   end
 
   def posts
@@ -16,6 +19,10 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
   end
 
 end
