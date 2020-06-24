ed212b47b79cf5600085d69fd944238b1f6771a8
Flora Petersen <floramarina@comcast.net>
2020-06-24 22:31:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name
 
+  @@song_count = 0
+
   def initialize(name)
     @name = name
   end
@@ -11,10 +13,12 @@ class Artist
 
   def songs
     Song.all.select {|song| song.artist == self}
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count += 1
   end
 end
